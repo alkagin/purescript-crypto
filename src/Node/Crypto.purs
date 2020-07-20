b94c52bcab7ed57a57d04356c51421e7ed882a08
Andrea Sella <me@alkagin.xyz>
2020-07-20 09:39:23 +0200

    add verify2

--- src/Node/Crypto.purs
+++ src/Node/Crypto.purs
@@ -1,14 +1,15 @@
 module Node.Crypto where
 
 import Prelude
-
 import Effect (Effect)
 import Node.Buffer (Buffer, fromString, toString)
 import Node.Crypto.Hash (Algorithm(..))
 import Node.Crypto.Hmac (createHmac, digest, update)
 import Node.Encoding (Encoding(..))
 
-timingSafeEqualString :: String -> String -> Effect Boolean
+foreign import data KeyObject ∷ Type
+
+timingSafeEqualString ∷ String -> String -> Effect Boolean
 timingSafeEqualString x1 x2 = do
   a1 <- fromString x1 UTF8
   a2 <- fromString x2 UTF8
@@ -17,6 +18,8 @@ timingSafeEqualString x1 x2 = do
   b2 <- createHmac SHA256 secret >>= flip update a2 >>= digest
   conj (x1 == x2) <$> timingSafeEqual b1 b2
 
-foreign import timingSafeEqual :: Buffer -> Buffer -> Effect Boolean
+foreign import timingSafeEqual ∷ Buffer -> Buffer -> Effect Boolean
 
-foreign import randomBytes :: Int -> Effect Buffer
+foreign import randomBytes ∷ Int -> Effect Buffer
+
+foreign import createPublicKey ∷ Buffer -> Effect KeyObject
