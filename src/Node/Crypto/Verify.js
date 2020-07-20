57d60575aaf78a958aaf016f7e7f0da7da2e94d9
Andrea Sella <me@alkagin.xyz>
2020-07-20 09:39:23 +0200

    add verify2

--- src/Node/Crypto/Verify.js
+++ src/Node/Crypto/Verify.js
@@ -3,23 +3,25 @@
 var crypto = require('crypto');
 
 exports._createVerify = function (algorithm) {
-    return function () {
-        crypto.createVerify(algorithm);
-    }
+  return function () {
+    return crypto.createVerify(algorithm);
+  }
 }
 
-exports._update = function(verify) {
-  return function(data) {
-    return function() {
-      verify.update(data);
+exports._update = function (verify) {
+  return function (data) {
+    return function () {
+      return verify.update(data);
     }
   }
 }
 
 exports._verify = function (verify) {
-  return function(object) {
-    return function(signature) {
-      verify.verify(object, signature);
+  return function (object) {
+    return function (signature) {
+      return function () {
+        return verify.verify(object, signature);
+      }
     }
   }
 }
--- src/Node/Crypto/Verify.purs
+++ src/Node/Crypto/Verify.purs
@@ -3,36 +3,43 @@ module Node.Crypto.Verify
   , Data
   , Signature
   , verify
+  , verify2
   ) where
 
 import Prelude
-
 import Data.Newtype (class Newtype, unwrap)
 import Effect (Effect)
 import Node.Buffer (Buffer)
 import Node.Crypto.Hash (Algorithm)
 
-foreign import data Verify :: Type
+foreign import data Verify ∷ Type
 
 newtype Object = Object Buffer
-derive instance ntObject :: Newtype Object _
-newtype Data   = Data Buffer
-derive instance ntData :: Newtype Data _
+
+derive instance ntObject ∷ Newtype Object _
+newtype Data = Data Buffer
+
+derive instance ntData ∷ Newtype Data _
 newtype Signature = Signature Buffer
-derive instance ntSignature :: Newtype Signature _
 
-verify :: Algorithm -> Object -> Data -> Signature -> Effect Boolean
+derive instance ntSignature ∷ Newtype Signature _
+verify2 ∷ Algorithm -> Buffer -> Buffer -> Buffer -> Effect Boolean
+verify2 alg object dat signature = do
+  createVerify alg >>= flip _update dat >>= flip (flip _verify object) signature
+
+verify ∷ Algorithm -> Object -> Data -> Signature -> Effect Boolean
 verify alg object dat signature = do
-    let objBuff = unwrap $ object
-        dataBuf = unwrap $ dat
-        signBuf = unwrap $ signature
-    createVerify alg >>= flip _update dataBuf >>= flip (flip _verify signBuf) objBuff
+  let
+    objBuff = unwrap $ object
+    dataBuf = unwrap $ dat
+    signBuf = unwrap $ signature
+  createVerify alg >>= flip _update dataBuf >>= flip (flip _verify objBuff) signBuf
 
-createVerify :: Algorithm -> Effect Verify
+createVerify ∷ Algorithm -> Effect Verify
 createVerify alg = _createVerify (show alg)
 
-foreign import _createVerify :: String -> Effect Verify
+foreign import _createVerify ∷ String -> Effect Verify
 
-foreign import _update :: Verify -> Buffer -> Effect Verify
+foreign import _update ∷ Verify -> Buffer -> Effect Verify
 
-foreign import _verify :: Verify -> Buffer -> Buffer -> Effect Boolean
+foreign import _verify ∷ Verify -> Buffer -> Buffer -> Effect Boolean
