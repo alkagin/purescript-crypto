0a7a7e9d1dc3d39ce1c22b41a8ee61c60495249c
Andrea Sella <me@alkagin.xyz>
2020-07-21 12:03:45 +0200

    stuff

--- src/Node/Crypto/Verify.purs
+++ src/Node/Crypto/Verify.purs
@@ -1,5 +1,5 @@
 module Node.Crypto.Verify
-  ( Object
+  ( StringOrBuffer
   , Data
   , Signature
   , verify
@@ -9,37 +9,37 @@ module Node.Crypto.Verify
 import Prelude
 import Data.Newtype (class Newtype, unwrap)
 import Effect (Effect)
-import Node.Buffer (Buffer)
+import Node.Crypto (KeyObject)
 import Node.Crypto.Hash (Algorithm)
 
 foreign import data Verify ∷ Type
 
-newtype Object = Object Buffer
-
-derive instance ntObject ∷ Newtype Object _
-newtype Data = Data Buffer
-
 derive instance ntData ∷ Newtype Data _
-newtype Signature = Signature Buffer
+newtype Data = Data StringOrBuffer
 
 derive instance ntSignature ∷ Newtype Signature _
-verify2 ∷ Algorithm -> Buffer -> Buffer -> Buffer -> Effect Boolean
+newtype Signature = Signature StringOrBuffer
+
+data StringOrBuffer
+  = String
+  | Buffer
+
+verify2 ∷ Algorithm -> KeyObject -> StringOrBuffer -> StringOrBuffer -> Effect Boolean
 verify2 alg object dat signature = do
   createVerify alg >>= flip _update dat >>= flip (flip _verify object) signature
 
-verify ∷ Algorithm -> Object -> Data -> Signature -> Effect Boolean
+verify ∷ Algorithm -> KeyObject -> Data -> Signature -> Effect Boolean
 verify alg object dat signature = do
   let
-    objBuff = unwrap $ object
     dataBuf = unwrap $ dat
     signBuf = unwrap $ signature
-  createVerify alg >>= flip _update dataBuf >>= flip (flip _verify objBuff) signBuf
+  createVerify alg >>= flip _update dataBuf >>= flip (flip _verify object) signBuf
 
 createVerify ∷ Algorithm -> Effect Verify
 createVerify alg = _createVerify (show alg)
 
 foreign import _createVerify ∷ String -> Effect Verify
 
-foreign import _update ∷ Verify -> Buffer -> Effect Verify
+foreign import _update ∷ Verify -> StringOrBuffer -> Effect Verify
 
-foreign import _verify ∷ Verify -> Buffer -> Buffer -> Effect Boolean
+foreign import _verify ∷ Verify -> KeyObject -> StringOrBuffer -> Effect Boolean
