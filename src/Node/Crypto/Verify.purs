617c0c0b4f7f3f4e6a6e8435f21daec98ed79880
Andrea Sella <me@alkagin.xyz>
2020-07-21 18:20:34 +0200

    lalala

--- src/Node/Crypto/Verify.purs
+++ src/Node/Crypto/Verify.purs
@@ -2,13 +2,12 @@ module Node.Crypto.Verify where
 
 import Prelude
 import Effect (Effect)
-import Node.Crypto (KeyObject)
 import Node.Buffer (Buffer)
 import Node.Crypto.Hash (Algorithm)
 
 foreign import data Verify ∷ Type
 
-verify ∷ Algorithm -> KeyObject -> String -> Buffer -> Effect Boolean
+verify ∷ Algorithm -> String -> String -> Buffer -> Effect Boolean
 verify alg object dat signature = do
   createVerify alg >>= flip _update dat >>= flip (flip _verify object) signature
 
@@ -19,4 +18,4 @@ foreign import _createVerify ∷ String -> Effect Verify
 
 foreign import _update ∷ Verify -> String -> Effect Verify
 
-foreign import _verify ∷ Verify -> KeyObject -> Buffer -> Effect Boolean
+foreign import _verify ∷ Verify -> String -> Buffer -> Effect Boolean
